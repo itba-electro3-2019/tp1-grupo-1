63c6e29e3e20d45681f08a4ba768087d9e6be620
Facundo Farall <ffarall@github.com>
2019-08-22 15:01:19 -0300

    Adding EJ_1

--- EJ_3/src/decoder4out.v
+++ EJ_3/src/decoder4out.v
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 module decoder4out(coded, y0, y1, y2, y3);
     input [1:0] coded;
     output y0, y1, y2, y3;
@@ -8,3 +9,8 @@ module decoder4out(coded, y0, y1, y2, y3);
     assign y3 = coded[1] & coded[0];
 
 endmodule
+=======
+module decoder4out(x1, x2, y1, y2, y3, y4);
+    input x1, x2;
+    output y1, y2, y3, y4;
+>>>>>>> Adding EJ_1

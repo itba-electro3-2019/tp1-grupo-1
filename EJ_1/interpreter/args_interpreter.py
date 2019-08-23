c1690c2bb352ab8f3c4936d66f245ac23265ffda
Facundo Farall <ffarall@github.com>
2019-08-23 13:20:02 -0300

    Program working, missing test bench

--- EJ_1/interpreter/args_interpreter.py
+++ EJ_1/interpreter/args_interpreter.py
@@ -14,17 +14,31 @@ class ArgumentsInterpreter():
 
     def __init__(self, arg_list: list):
         self.argument_list = arg_list
+        self.err_message = []
         self.is_valid = self.validate()
 
     def validate(self):
-        self.is_valid = len(self.argument_list) == 3 and all(isinstance(x, int) for x in self.argument_list) and (self.argument_list[0] == 0 or self.argument_list[0] == 0)
+        validated = True
+        if len(self.argument_list) != 3:
+            self.err_message.append('Argument list\' size different from 3.')
+            validated = False
+        if not all(isinstance(x, int) for x in self.argument_list):
+            self.err_message.append('Argument list\' elements are not int.')
+            validated = False
+        if (self.argument_list[0] != 0 and self.argument_list[0] != 1):
+            self.err_message.append('Argument list\' first element is neither 0 nor 1.')
+            validated = False
+        return validated
 
     def get_resolution(self):
         return 2 ** -self.argument_list[2]
 
     def get_range(self):
-        return 2 ** self.argument_list[1] - 2 ** self.argument_list[2]
+        return 2 ** self.argument_list[1] - 2 ** -self.argument_list[2]
         '''if (self.argument_list[0]):
             return 2 ** (self.argument_list[1] + self.argument_list[2])
         else:
             return 2 ** (self.argument_list[1] + self.argument_list[2]) - 1''' 
+
+    def get_err_message(self):
+        return self.err_message
--- EJ_1/main.py
+++ EJ_1/main.py
@@ -1,9 +1,9 @@
 import sys
 from interpreter.args_interpreter import ArgumentsInterpreter
 
-my_interpreter = ArgumentsInterpreter(sys.argv)       # list with parameters from command line, should be something like [0, 3, 5]
+my_interpreter = ArgumentsInterpreter(list(map(int, sys.argv[1:])))       # list with parameters from command line, should be something like [0, 3, 5]
 
 if my_interpreter.is_valid:
     print('Res: {} | Ran: {}'.format(my_interpreter.get_resolution(), my_interpreter.get_range()))
 else:
-    print('FAILURE')
+    print(*my_interpreter.get_err_message(), sep = '\n')

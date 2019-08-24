17527b402c28d8af8429cae34c53ae468470a0aa
Facundo Farall <ffarall@github.com>
2019-08-23 22:45:10 -0300

    More successful tests added

--- EJ_1/main.py
+++ EJ_1/main.py
@@ -1,9 +1,19 @@
 import sys
 from interpreter.args_interpreter import ArgumentsInterpreter
 
-my_interpreter = ArgumentsInterpreter(sys.argv[1:])       # list with parameters from command line, should be something like [0, 3, 5]
+if len(sys.argv) > 1:
+    print('Arguments were', str(sys.argv[1:]))
+    
+    my_interpreter = ArgumentsInterpreter(sys.argv[1:])       # list with parameters from command line, should be something like [0, 3, 5]
 
-if my_interpreter.is_valid:
-    print('Res: {} | Ran: {}'.format(my_interpreter.get_resolution(), my_interpreter.get_range()))
+    if my_interpreter.is_valid:
+        print('Res: {} | Ran: {}'.format(my_interpreter.get_resolution(), my_interpreter.get_range()))
+        print('')
+    else:
+        print('Errors are:')
+        print(*my_interpreter.get_err_message(), sep = '\n')
+        print('')
+
 else:
-    print(*my_interpreter.get_err_message(), sep = '\n')
+    print('No arguments passed.')
+    print('')
--- EJ_1/run.sh
+++ EJ_1/run.sh
@@ -2,3 +2,6 @@
 
 python main.py 0 1 1
 python main.py o hola 1
+python main.py 011
+python main.py 1 0 0
+python main.py 
